c8342a08cdc6c3cb776c379ccc1164b8934fade4
Aditya_Contributes <146584952+Dev-Aditya-More@users.noreply.github.com>
2025-06-20 17:07:56 +0530

    nothing

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -46,6 +46,8 @@ const projects = [
     description: 'Android app to scan QR codes using ML Kit and CameraX.',
     longDesc: 'Lightweight scanner using Google ML Kits Barcode API, with real-time decoding and camera control using CameraX. Kotlin-based implementation.',
     image: 'https://images.unsplash.com/photo-1590479773265-7464e5d48118?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+    tags: [],
+    github: ''
 ];
 
 const ProjectsSection = () => {
