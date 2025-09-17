35da09bce71ff86dd358b95489914de063c15718
Aditya More <aditya1875more@gmail.com>
2025-09-18 00:36:48 +0530

    resume tweaks

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -53,7 +53,7 @@ const Navbar = () => {
             </a>
           ))}
           <ThemeToggle />
-          <a href="/Aditya_More_New_1.pdf" download>
+          <a href="/Aditya_Resume.pdf" download>
   <Button>Resume</Button>
 </a>
 
@@ -83,7 +83,7 @@ const Navbar = () => {
                   {item.name}
                 </a>
               ))}
-              <a href="/Aditya_More_New_1.pdf" download className="w-full"> <Button className="w-full">Resume</Button></a>
+              <a href="/Aditya_Resume" download className="w-full"> <Button className="w-full">Resume</Button></a>
             </div>
           </div>
         )}
